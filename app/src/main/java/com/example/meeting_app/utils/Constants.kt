3da7856f4f522130a00ed19506f019e6755e24f4
imufiid <imufiid98@gmail.com>
2021-05-24 09:24:59 +0700

    update local base url

--- app/src/main/java/com/example/meeting_app/utils/Constants.kt
+++ app/src/main/java/com/example/meeting_app/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.meeting_app.utils
 
 object Constants {
-    const val API_ENDPOINT = "http://localhost/projects/notulen/index.php/api/"
+    const val API_ENDPOINT = "http://192.168.1.4/projects/notulen/index.php/api/"
 }
