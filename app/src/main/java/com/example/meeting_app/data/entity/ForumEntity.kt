d514994044e733090d9b913bda115b7af22481e1
imufiid <imufiid98@gmail.com>
2021-05-24 09:29:54 +0700

    forum entity

---
+++ app/src/main/java/com/example/meeting_app/data/entity/ForumEntity.kt
@@ -0,0 +1,36 @@
+package com.example.meeting_app.data.entity
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ForumEntity(
+
+	@field:SerializedName("likes_count")
+	val likesCount: Int? = null,
+
+	@field:SerializedName("waktu")
+	val waktu: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("id_rapat")
+	val idRapat: String? = null,
+
+	@field:SerializedName("id_user")
+	val idUser: String? = null,
+
+	@field:SerializedName("isi")
+	val isi: String? = null,
+
+	@field:SerializedName("suka")
+	val suka: String? = null,
+
+	@field:SerializedName("likes")
+	val likes: List<String?>? = null,
+
+	@field:SerializedName("total_reply")
+	val totalReply: Int? = null
+) : Parcelable
