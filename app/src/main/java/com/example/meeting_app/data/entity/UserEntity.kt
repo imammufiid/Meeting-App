d64a026708b86c987ed9fe22c430794ae3e64bed
imufiid <imufiid98@gmail.com>
2021-05-24 09:19:19 +0700

    updated user entity

--- app/src/main/java/com/example/meeting_app/data/entity/UserEntity.kt
+++ app/src/main/java/com/example/meeting_app/data/entity/UserEntity.kt
@@ -7,33 +7,48 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class UserEntity(
 
-	@field:SerializedName("firstname")
-    var firstName: String? = null,
+	@field:SerializedName("image")
+	val image: String? = null,
 
-	@field:SerializedName("updated_at")
-	val updatedAt: String? = null,
+	@field:SerializedName("is_active")
+	val isActive: String? = null,
 
-	@field:SerializedName("role_id")
-	val roleId: Int? = null,
+	@field:SerializedName("level")
+	val level: String? = null,
 
-	@field:SerializedName("access_id")
-	val accessId: Int? = null,
+	@field:SerializedName("jabatan")
+	val jabatan: String? = null,
 
-	@field:SerializedName("created_at")
-	val createdAt: String? = null,
+	@field:SerializedName("date_created")
+	val dateCreated: String? = null,
 
-	@field:SerializedName("id")
-	var id: Int? = null,
+	@field:SerializedName("pangkat")
+	val pangkat: String? = null,
+
+	@field:SerializedName("id_user")
+	var idUser: String? = null,
+
+	@field:SerializedName("password")
+	val password: String? = null,
+
+	@field:SerializedName("fax_instansi")
+	val faxInstansi: String? = null,
+
+	@field:SerializedName("nama")
+	var nama: String? = null,
+
+	@field:SerializedName("nip")
+	val nip: String? = null,
+
+	@field:SerializedName("alamat_instansi")
+	val alamatInstansi: String? = null,
+
+	@field:SerializedName("telepon_instansi")
+	val teleponInstansi: String? = null,
 
 	@field:SerializedName("email")
 	var email: String? = null,
 
-	@field:SerializedName("username")
-	var username: String? = null,
-
-	@field:SerializedName("lastname")
-	var lastName: String? = null,
-
-	@field:SerializedName("token")
-	var token: String? = null
-): Parcelable
+	@field:SerializedName("instansi")
+	val instansi: String? = null
+) : Parcelable
